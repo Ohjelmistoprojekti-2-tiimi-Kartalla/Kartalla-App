9b864ca61e31482a7af70b82fad72e371317a096
Matias Nisonen <m.nisonen@gmail.com>
2025-09-30 12:51:29 +0300

    DestinationDetailsScreen styling and added image to assets

--- screens/DestinationDetailsScreen.tsx
+++ screens/DestinationDetailsScreen.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import { styles } from "../styles";
+import { View, Text, StyleSheet, Image } from 'react-native';
 import { RouteProp } from "@react-navigation/native";
 import { Location } from "../types/Location";
 
@@ -18,16 +17,55 @@ type DestinationDetailsRouteProp = RouteProp<
 interface Props {
   route: DestinationDetailsRouteProp;
 }
-const DestinationDetailsScreen: React.FC<Props> = ({ route }: any) => {
+
+const DestinationDetailsScreen: React.FC<Props> = ({ route }) => {
   const { location } = route.params;
 
   return (
     <View style={styles.container}>
-      <Text style={styles.h1}>
-        {location.name}
-      </Text>
+      {/* Otsikko ylös */}
+      <Text style={styles.title}>{location.name}</Text>
+
+      {/* Header-kuvat */}
+      <View style={styles.imageRow}>
+        <Image
+          source={require('../assets/maisema.png')}
+          style={styles.headerImage}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0E1815', 
+    padding: 16,
+  },
+  imageRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+  headerImage: {
+    width: '100%',
+    height: 180,
+    borderRadius: 12,
+    marginBottom: 16,
+    alignSelf: 'center',
+    resizeMode: 'cover',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 18,           
+    fontWeight: 'bold',
+    marginTop: 10,          
+    marginBottom: 16,
+    textAlign: 'center',    
+    alignSelf: 'center',
+    fontFamily: 'serif', // <-- lisää tämä rivi
+  },
+});
 
 export default DestinationDetailsScreen;
